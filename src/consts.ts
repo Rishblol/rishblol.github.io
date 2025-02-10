def4a427676bc09277fbb60f0642f795f4c8f658
Rishblol <rish90093@gmail.com>
2025-02-10 15:00:11 +0530

    update project

--- src/consts.ts
+++ src/consts.ts
@@ -24,7 +24,7 @@ export const SITE: Site = {
 
 export const NAV_LINKS: Link[] = [
   { href: '/blog', label: 'blog' },
-  { href: '/about', label: 'about' }
+  { href: '/projects', label: 'projects' }
   
 ]
 
