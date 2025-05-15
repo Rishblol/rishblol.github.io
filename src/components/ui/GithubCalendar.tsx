9eb96bfda311c0d97fa91ec489464eebe2346883
Rishblol <rish90093@gmail.com>
2025-05-15 14:15:16 +0530

    more additions innit

--- src/components/ui/GithubCalendar.tsx
+++ src/components/ui/GithubCalendar.tsx
@@ -67,7 +67,7 @@ const GithubCalendar: FunctionComponent<Props> = ({ username, ...props }) => {
     <>
       <div className="m-4 hidden sm:block">
         <Calendar
-          data={selectLastNDays(data.contributions,165)}
+          data={selectLastNDays(data.contributions,200)}
           theme={{
             dark: ['#1A1A1A', '#10B981'],
           }}
