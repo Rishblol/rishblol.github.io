59be03393f4cbe19b71db3091abd84d9c25bf50c
Rishblol <rish90093@gmail.com>
2025-05-19 18:26:32 +0530

    layout change

--- src/components/ui/GithubCalendar.tsx
+++ src/components/ui/GithubCalendar.tsx
@@ -67,7 +67,7 @@ const GithubCalendar: FunctionComponent<Props> = ({ username, ...props }) => {
     <>
       <div className="m-4 hidden sm:block">
         <Calendar
-          data={selectLastNDays(data.contributions,96)}
+          data={selectLastNDays(data.contributions,198)}
           theme={{
             dark: ['#1A1A1A', '#10B981'],
           }}
